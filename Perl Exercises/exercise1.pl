369544a02abef7b6a56679060fd205f856806310
Dani2000 <75886396+Dani285@users.noreply.github.com>
2021-11-25 02:46:51 +0100

    Add files via upload

---
+++ Perl Exercises/exercise1.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl -w
+for ($i=0;$i<10;$i++)     # lt- less than(<) this loop handles the rows
+{		                 
+ for($k=10;$k>$i+1;$k--)      # this loop handles spaces
+ {
+	print " ";		
+ }
+ for($j=0;$j<=$i;$j++)#le-less or equal than(<=) this loop handles the stars
+ {
+    print "* ";
+ }
+ print "\n";
+}
+for ($i=0;$i<1;$i++)     
+{		                 
+ for($k=10;$k>$i+2;$k--)      
+ {
+	print " ";		
+ }
+ for($f=0;$f<3;$f++)
+ {
+     print "* ";
+ }
+}
+print "\n";
+for ($i=0;$i<1;$i++)     
+{		                 
+ for($k=10;$k>$i+2;$k--)      
+ {
+	print " ";		
+ }
+ for($h=0;$h<3;$h++)
+ {
+     print "* ";
+ }
+}
+print "\n";
+for ($i=0;$i<1;$i++)     
+{		                 
+ for($k=10;$k>$i+2;$k--)      
+ {
+	print " ";		
+ }
+ for($f=0;$f<3;$f++)
+ {
+     print "* ";
+ }
+}
+print "\n";
+for ($i=0;$i<1;$i++)     
+{		                 
+ for($k=10;$k>$i+2;$k--)      
+ {
+	print " ";		
+ }
+ for($m=0;$m<3;$m++)
+ {
+     print "* ";
+ }
+}
+print "\n";
+for ($i=0;$i<1;$i++)     
+{		                 
+ for($k=10;$k>$i+9;$k--)      
+ {
+	print " ";		
+ }
+ for($q=0;$q<10;$q++)
+ {
+     print "* ";
+ }
+}
