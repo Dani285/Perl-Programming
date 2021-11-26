866d22f4f6c1e6c35cba86da37e0ebffb70845f7
Dani2000 <75886396+Dani285@users.noreply.github.com>
2021-11-26 19:35:40 +0100

    Update exercise2.pl

--- Perl Exercises/exercise2.pl
+++ Perl Exercises/exercise2.pl
@@ -2,6 +2,7 @@
 #filter the email addresses within text file
 #regex: ~ /^[^@]+@+[^\.]+\.+[^\.]{2,6}$/
 #$file = "C:\\Users\\kaloc\\Desktop\\Perl\\input.txt";
+#Solution for Task 2
 
 open(FH,'<', 'input.txt') or die ("Couldn't open the input.txt file");
 while(<FH>) {
