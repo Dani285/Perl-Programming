2022bf64ed7b8d92d5a8c59401af8b6a2907886d
Dani2000 <75886396+Dani285@users.noreply.github.com>
2021-11-27 05:25:44 +0100

    Add files via upload

---
+++ Perl Exercises/exercise9.pl
@@ -0,0 +1,51 @@
+#!/usr/ bin/perl -w
+
+use strict;
+use warnings;
+use List::Util qw(shuffle);        #this is an alternative solution
+
+#Practice foreign words
+if ($#ARGV != 0){ 
+    print "Usage $0 <words.txt>\n";
+    exit(8);
+}
+open IN, "<$ARGV[0]" or die("Could not open $ARGV[0] for reading");
+
+my @list; # list of questions and answers
+
+
+while(<IN>){ 
+	chomp;
+	my @words= split /\t/;
+	if($#words != 1){
+	   die("Wrong input $_");
+	}
+	push(@list,
+	{
+	 questions => $words[0],
+	 answers => $words[1],
+	});
+}
+# Question-Answer
+#Ask the question until there are no more
+
+while ($#list>-1){
+	@list = shuffle(@list);
+	print "Translate to Spanish: $list[0]->{questions}\n";
+	print "My answer:";
+	my $answers = <STDIN>; 
+	chomp($answers);
+	   if ($answers eq $list[0]->{answers}) 
+	      {
+		print "Right answer the answer was $list[0]->{answers}\n\n";
+		shift(@list);
+		next;
+	      }else{
+	      	print "Wrong answer the correct answer is: $list[0]->{answers}\n\n";
+	        push(@list, shift(@list));
+	        until($answers ne $list[0]->{answers}){
+	           push(@list,$list[0]->{questions});
+	        }
+	    }
+}
+print "That was for today all done!\n";
