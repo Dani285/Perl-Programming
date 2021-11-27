435756f79298d491b402998e7297aa2538c6df5c
Dani2000 <75886396+Dani285@users.noreply.github.com>
2021-11-27 05:30:12 +0100

    Update exercise9.pl

--- Perl Exercises/exercise9.pl
+++ Perl Exercises/exercise9.pl
@@ -1,9 +1,9 @@
 #!/usr/ bin/perl -w
 
 #The usage of this script:
-1. Open Terminal
-2. Download words.txt file
-3. Type the following to the terminal: perl exercise9.pl words.txt
+#1. Open Terminal
+#2. Download words.txt file
+#3. Type the following to the terminal: perl exercise9.pl words.txt
 
 use strict;
 use warnings;
